d34dbbd72a7e7de981fe567d18ca9f55609f6d7a
LanceMorii <15035267995@163.com>
2025-08-26 11:58:51 +0800

    继承

---
+++ java_code/src/opextend/opextenddemo2/HaShiQi.java
@@ -0,0 +1,12 @@
+package opextend.opextenddemo2;
+
+/**
+ * @Author: Maven
+ * @Date: 2025/8/26 11:52 （日期和时间）
+ * @OOTD: 哈士奇子类
+ */
+public class HaShiQi extends  Dog{
+    public void breakHome () {
+        System.out.println("哈士奇在拆家");
+    }
+}
